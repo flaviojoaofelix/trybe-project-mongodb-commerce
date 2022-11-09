2a74476a13a2d104482ac95229730407b603ae48
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-11-09 18:03:15 -0300

    :sparkles: feat: requirements 04

---
+++ challenges/desafio4.js
@@ -0,0 +1,8 @@
+// Retorne os lanches que tiveram vendas maiores que 50 e menores que 100,
+// mostrando apenas o nome e a quantidade de lanches vendidos em ordem crescente
+db.produtos
+  .find(
+    { vendidos: { $gt: 50, $lt: 100 } },
+    { _id: false, nome: true, vendidos: true }
+  )
+  .sort({ vendidos: 1 });
