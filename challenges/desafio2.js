3587637f240eb3082329ab005ccc74dcfc7d38c8
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-11-09 18:02:58 -0300

    :sparkles: feat: requirements 02

---
+++ challenges/desafio2.js
@@ -0,0 +1,12 @@
+// 2 - Ordene a coleção produtos pela quantidade de lanches vendidos em ordem crescente,
+// mostrando apenas o nome e a quantidade de lanches vendidos
+db.produtos
+  .find(
+    {},
+    {
+      _id: false,
+      nome: true,
+      vendidos: true,
+    }
+  )
+  .sort({ vendidos: 1 });
