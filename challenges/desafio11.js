0cdbe39ca445a5ce162d1c0df7f83e81cc7cb95e
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-11-10 00:15:15 -0300

    :sparkles: feat: requirements 11

---
+++ challenges/desafio11.js
@@ -0,0 +1,6 @@
+// Retorne o nome do produto, a quantidade de curtidas e quantos itens foram
+// vendidos dos produtos que não sejam iguais a Big Mac e McChicken
+db.produtos.find(
+  { nome: { $nin: ["Big Mac", "McChicken"] } },
+  { _id: false, nome: true, curtidas: true, vendidos: true },
+); // Questao 11
