74c40b9854e9d708e563bff5510043a1e22dbd04
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-11-10 01:35:11 -0300

    :sparkles: feat: requirements 18

---
+++ challenges/desafio18.js
@@ -0,0 +1,19 @@
+// Inclua bacon no final da lista de ingredientes dos sanduíches Big Mac e
+// Quarteirão com Queijo
+db.produtos.updateMany(
+  {
+    nome: {
+      $in: ["Big Mac", "Quarteirão com Queijo"],
+    },
+  },
+  {
+    $push: { ingredientes: "bacon" },
+  },
+);
+db.produtos.find({},
+  {
+    _id: false,
+    nome: true,
+    ingredientes: true
+  },
+);
