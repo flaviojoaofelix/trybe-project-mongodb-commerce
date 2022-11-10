f9dab59e439602a1704dd8727ba2fa82cf34f4b2
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-11-10 01:07:46 -0300

    :sparkles: feat: requirements 15

---
+++ challenges/desafio15.js
@@ -0,0 +1,18 @@
+// Adicione o campo avaliacao em todos os documentos da coleção e efetue
+//  alterações nesse campo
+db.produtos.updateMany(
+  {},
+  { $set: { avaliacao: 0 } },
+);
+db.produtos.updateMany(
+  { tags: { $in: ["bovino"] } },
+  { $set: { avaliacao: 5 } },
+);
+db.produtos.updateMany(
+  { tags: { $in: ["ave"] } },
+  { $set: { avaliacao: 3 } },
+);
+db.produtos.find(
+  {},
+  { _id: false, nome: true, avaliacao: true },
+); // Questao 15
