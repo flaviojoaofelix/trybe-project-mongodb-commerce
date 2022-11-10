4861027d0411b44cc4acbc897f37746b6c8dd488
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-11-10 00:14:33 -0300

    :sparkles: feat: requirements 09

---
+++ challenges/desafio9.js
@@ -0,0 +1,13 @@
+// Retorne o nome de todos os lanches que possuam calorias abaixo de 500
+db.produtos.find(
+  {
+    $and: [
+      {
+        valoresNutricionais: {
+          $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } },
+        },
+      },
+    ],
+  },
+  { _id: false, nome: true },
+);
