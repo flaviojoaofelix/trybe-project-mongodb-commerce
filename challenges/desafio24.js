39f731926aff19d92abdfaab9fafe10a4bcd3be6
Flávio João Félix <flaviojoao.felix@gmail.com>
2022-11-10 11:40:27 -0300

    :sparkles: feat: requirement 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,14 @@
+// Ordene em todos os documentos os valores do array valoresNutricionais pelo
+// campo percentual de forma decrescente
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      valoresNutricionais: {
+        $each: [],
+        $sort: { percentual: -1 },
+      },
+    },
+  }
+);
+db.produtos.find({}, { _id: false, nome: true, valoresNutricionais: true });
